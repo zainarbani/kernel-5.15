4805b8a90c884de7bfbebb1210fff8d4c74a1b4b
Eric Biggers <ebiggers@google.com>
2025-02-07 18:49:09 -0800

    lib/crc32: standardize on crc32c() name for Castagnoli CRC32
    For historical reasons, the Castagnoli CRC32 is available under 3 names:
    crc32c(), crc32c_le(), and __crc32c_le().  Most callers use crc32c().
    The more verbose versions are not really warranted; there is no "_be"
    version that the "_le" version needs to be differentiated from, and the
    leading underscores are pointless.
    
    Therefore, let's standardize on just crc32c().  Remove the other two
    names, and update callers accordingly.
    
    Specifically, the new crc32c() comes from what was previously
    __crc32c_le(), so compared to the old crc32c() it now takes a size_t
    length rather than unsigned int, and it's now in linux/crc32.h instead
    of just linux/crc32c.h (which includes linux/crc32.h).
    
    Later patches will also rename __crc32c_le_combine(), crc32c_le_base(),
    and crc32c_le_arch().
    
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lore.kernel.org/r/20250208024911.14936-5-ebiggers@kernel.org
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- drivers/md/raid5-cache.c
+++ drivers/md/raid5-cache.c
@@ -716,7 +716,7 @@ static void r5l_submit_current_io(struct r5l_log *log)
 
 	block = page_address(io->meta_page);
 	block->meta_size = cpu_to_le32(io->meta_offset);
-	crc = crc32c_le(log->uuid_checksum, block, PAGE_SIZE);
+	crc = crc32c(log->uuid_checksum, block, PAGE_SIZE);
 	block->checksum = cpu_to_le32(crc);
 
 	log->current_io = NULL;
@@ -1023,7 +1023,7 @@ int r5l_write_stripe(struct r5l_log *log, struct stripe_head *sh)
 		if (test_bit(STRIPE_LOG_TRAPPED, &sh->state))
 			continue;
 		addr = kmap_atomic(sh->dev[i].page);
-		sh->dev[i].log_checksum = crc32c_le(log->uuid_checksum,
+		sh->dev[i].log_checksum = crc32c(log->uuid_checksum,
 						    addr, PAGE_SIZE);
 		kunmap_atomic(addr);
 	}
@@ -1750,7 +1750,7 @@ static int r5l_recovery_read_meta_block(struct r5l_log *log,
 	    le64_to_cpu(mb->position) != ctx->pos)
 		return -EINVAL;
 
-	crc = crc32c_le(log->uuid_checksum, mb, PAGE_SIZE);
+	crc = crc32c(log->uuid_checksum, mb, PAGE_SIZE);
 	if (stored_crc != crc)
 		return -EINVAL;
 
@@ -1789,7 +1789,7 @@ static int r5l_log_write_empty_meta_block(struct r5l_log *log, sector_t pos,
 		return -ENOMEM;
 	r5l_recovery_create_empty_meta_block(log, page, pos, seq);
 	mb = page_address(page);
-	mb->checksum = cpu_to_le32(crc32c_le(log->uuid_checksum,
+	mb->checksum = cpu_to_le32(crc32c(log->uuid_checksum,
 					     mb, PAGE_SIZE));
 	if (!sync_page_io(log->rdev, pos, PAGE_SIZE, page, REQ_OP_WRITE,
 			  REQ_SYNC | REQ_FUA, false)) {
@@ -1990,7 +1990,7 @@ r5l_recovery_verify_data_checksum(struct r5l_log *log,
 
 	r5l_recovery_read_page(log, ctx, page, log_offset);
 	addr = kmap_atomic(page);
-	checksum = crc32c_le(log->uuid_checksum, addr, PAGE_SIZE);
+	checksum = crc32c(log->uuid_checksum, addr, PAGE_SIZE);
 	kunmap_atomic(addr);
 	return (le32_to_cpu(log_checksum) == checksum) ? 0 : -EINVAL;
 }
@@ -2393,7 +2393,7 @@ r5c_recovery_rewrite_data_only_stripes(struct r5l_log *log,
 					raid5_compute_blocknr(sh, i, 0));
 				addr = kmap_atomic(dev->page);
 				payload->checksum[0] = cpu_to_le32(
-					crc32c_le(log->uuid_checksum, addr,
+					crc32c(log->uuid_checksum, addr,
 						  PAGE_SIZE));
 				kunmap_atomic(addr);
 				sync_page_io(log->rdev, write_pos, PAGE_SIZE,
@@ -2406,8 +2406,8 @@ r5c_recovery_rewrite_data_only_stripes(struct r5l_log *log,
 			}
 		}
 		mb->meta_size = cpu_to_le32(offset);
-		mb->checksum = cpu_to_le32(crc32c_le(log->uuid_checksum,
-						     mb, PAGE_SIZE));
+		mb->checksum = cpu_to_le32(crc32c(log->uuid_checksum,
+						  mb, PAGE_SIZE));
 		sync_page_io(log->rdev, ctx->pos, PAGE_SIZE, page,
 			     REQ_OP_WRITE, REQ_SYNC | REQ_FUA, false);
 		sh->log_start = ctx->pos;
@@ -2902,7 +2902,7 @@ int r5c_cache_data(struct r5l_log *log, struct stripe_head *sh)
 		if (!test_bit(R5_Wantwrite, &sh->dev[i].flags))
 			continue;
 		addr = kmap_atomic(sh->dev[i].page);
-		sh->dev[i].log_checksum = crc32c_le(log->uuid_checksum,
+		sh->dev[i].log_checksum = crc32c(log->uuid_checksum,
 						    addr, PAGE_SIZE);
 		kunmap_atomic(addr);
 		pages++;
@@ -2987,7 +2987,7 @@ static int r5l_load_log(struct r5l_log *log)
 	}
 	stored_crc = le32_to_cpu(mb->checksum);
 	mb->checksum = 0;
-	expected_crc = crc32c_le(log->uuid_checksum, mb, PAGE_SIZE);
+	expected_crc = crc32c(log->uuid_checksum, mb, PAGE_SIZE);
 	if (stored_crc != expected_crc) {
 		create_super = true;
 		goto create;
@@ -3098,7 +3098,7 @@ int r5l_init_log(struct r5conf *conf, struct md_rdev *rdev)
 
 	log->need_cache_flush = test_bit(QUEUE_FLAG_WC, &q->queue_flags) != 0;
 
-	log->uuid_checksum = crc32c_le(~0, rdev->mddev->uuid,
+	log->uuid_checksum = crc32c(~0, rdev->mddev->uuid,
 				       sizeof(rdev->mddev->uuid));
 
 	mutex_init(&log->io_mutex);
